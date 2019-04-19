1488ea406b40383099ea97ac2b5c2b7472acba2c
TiagoDanin <TiagoDanin@outlook.com>
2019-04-19 12:47:00 -0300

    feat: create nuxt app

---
+++ example/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+	<main>
+		<nuxt/>
+	</main>
+</template>
+
+<script>
+	export default {}
+</script>
---
+++ example/nuxt.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+	rootDir: path.resolve(__dirname, '..'),
+	buildDir: path.resolve(__dirname, '.nuxt'),
+	srcDir: __dirname,
+	render: {
+		resourceHints: false
+	},
+	modules: [
+		require('../index')
+	]
+}
